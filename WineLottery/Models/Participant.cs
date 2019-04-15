f48bd1f30df7ebc4a7580826a9485ad3919a2733
Tuomas Hara <tuomash@gmail.com>
2019-04-15 12:09:05 +0300

    Fix erroneous jsonignore in Participant

--- WineLottery/Models/Participant.cs
+++ WineLottery/Models/Participant.cs
@@ -4,12 +4,10 @@ namespace WineLottery.Models
 {
     public class Participant
     {
-        [JsonIgnore]
         public string Id { get; set; }
         public string Name { get; set; }
         public string UserId { get; set; }
         public string DraftId { get; set; }
-        [JsonIgnore]
         public bool HasWon { get; set; }
     }
 }
